c0161717d3efac7fbd3210cea0d3e0fc57ef884d
BrianCoveney <coveneybrian@gmail.com>
2018-03-14 20:37:35 +0000

    Update what the  ps command should print:

--- ps.c
+++ ps.c
@@ -6,9 +6,10 @@
 // Per-process state
 enum procstate { UNUSED, EMBRYO, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 struct proc {
+  int pid;                     // Process ID
+  int ppid ;                   // Parent process ID
   enum procstate state;        // Process state
-  int pid;            // Process ID
-  int ppid ;          // Parent process ID
+  uint sz;                     // Process size
   char name[16];               // Process name 
 };
 
@@ -25,30 +26,30 @@ main(int argc, char *argv[]){
     printf(1,"Error getting ptable");
   
   p = &ptable[0];
-  printf(1, " PID  PPID  STATE  CMD \n");
+  printf(1, " PID  PPID  STATE  CMD SIZE\n");
   while(p != &ptable[MAX_PROC-1] && p->state != UNUSED){
-  	printf(1,"%d  %d ",p->pid,p->ppid);
+  	printf(1," %d     %d  ",p->pid,p->ppid);
   	switch(p->state){
   	case UNUSED:
-  		printf(1," %s ", "UNUSED");
+  		printf(1,"  %s ", "UNUSED");
   		break;
   	case EMBRYO:
-  		printf(1," %s ", "EMBRYO");
+  		printf(1,"  %s ", "EMBRYO");
   		break;
   	case SLEEPING:
-  		printf(1," %s ", "SLEEPING");
+  		printf(1,"  %s ", "SLEEPING");
   		break;
   	case RUNNABLE:
-  		printf(1," %s ", "RUNNABLE");
+  		printf(1,"  %s ", "RUNNABLE");
   		break;
   	case RUNNING:
-  		printf(1," %s ", "RUNNING");
+  		printf(1,"  %s ", "RUNNING");
   		break;
   	case ZOMBIE:
-  		printf(1," %s ", "ZOMBIE");
+  		printf(1,"  %s ", "ZOMBIE");
   		break;
   	} 
-  	printf(1," %s \n", p->name);
+  	printf(1," %s  %d \n", p->name, p->sz);
   	p++;
   }
   	  
--- sysproc.c
+++ sysproc.c
@@ -137,6 +137,8 @@ int sys_getptable(void){
     s+=4;
     *(int *)s = p->parent->pid;
     s+=4;
+    *(int *)s = p->sz;
+    s+=4;
     memmove(s,p->name,16);
     s+=16;
     p++;
